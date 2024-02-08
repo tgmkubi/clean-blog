a28e6043586462614b23136cae2dc4df700230b2
Kubilay Uysal <kubilayuysall@gmail.com>
2024-02-08 12:35:06 +0300

    Static File & Ejs Template Engine Config

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Static Files
-const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'temp')));
+// Template Engine
+app.set('view engine', 'ejs');
 
+// Express Static Files MIDDLEWARE
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.status(200).send(blog);
+  res.render('index'); // ejs default olarak views klasöründe index isimli ejs dosyasını arar ve render eder.
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render("add_post");
+});
+
+app.get('/post', (req, res) => {
+  res.render("post");
 });
 
 app.listen(PORT, () => {
